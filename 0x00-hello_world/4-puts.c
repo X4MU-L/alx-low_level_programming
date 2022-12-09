0fa95e665450f5948457c8476905b4fec50d5806
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2022-12-08 16:21:05 -0800

    print to stdout using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,12 @@
  * main - Entry point
  *
  * Description: Prints to the stdout using puts
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char my_prnt[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(my_prnt);
 	return (0);
 }
