6398fdb19fd2e73dd82869d3d5f23288a9bdb460
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2022-12-09 18:27:12 -0800

    print int using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all lowercase and uppercase letters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i % 10 + '0');
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
