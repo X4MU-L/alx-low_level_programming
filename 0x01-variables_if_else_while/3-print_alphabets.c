90abcd60c316ec12700149f2ef3dbd6cde75a177
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2022-12-09 17:47:17 -0800

    print all characters in lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all lowercase and uppercase letters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+	i = 97;
+	j = 65;
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar(10);
+	return (0);
+}
