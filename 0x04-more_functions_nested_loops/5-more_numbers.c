33ef323abbf594b24630b96868d5962e8be34a48
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2022-12-15 15:41:23 -0800

    print all numbers from 0 to 14 and a newline 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Description: Prints all digits from 0 to 14 and a newline 10 times
+ *
+ * Return: Return (void)
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar((j / 10) + '0');
+
+			_putchar((j % 10) + '0');
+		}
+
+		_putchar(10);
+	}
+
+}
