3dea2d872bd20e9d7f12ed17e237b76aae1793b5
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2022-12-15 15:00:23 -0800

    multiply two given integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/**
+ * mul - Entry point
+ *
+ * @a: int to multiply
+ * @b: int to multiply
+ * Description: Multiplies a and b
+ *
+ * Return: Returns the result of multiplying a and b
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
