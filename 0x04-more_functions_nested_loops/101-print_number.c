595a85225012db2957a4b236b981f208ee225c27
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2022-12-15 18:17:01 -0800

    print integers using _putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * @n: int - number to print
+ *
+ * Description: Prints n using _putchar
+ * Return: Return void
+ */
+
+void print_number(int n)
+{
+	int tmp;
+
+	if (n < 0)
+	{
+		tmp = n * -1;
+	}
+	else
+	{
+		tmp = n;
+	}
+
+	if (tmp >= 0 && tmp < 10)
+	{
+		if (n < 0)
+			_putchar('-');
+		_putchar(tmp % 10 + '0');
+	}
+	else if (tmp >= 10 && tmp < 100)
+	{
+		if (n < 0)
+			_putchar('-');
+		_putchar((tmp / 10) + '0');
+		_putchar(tmp % 10 + '0');
+	}
+	else if (tmp >= 100 && tmp < 1000)
+	{
+		if (n < 0)
+			_putchar('-');
+		_putchar((tmp / 100) + '0');
+		_putchar(((tmp / 10) % 10) + '0');
+		_putchar(tmp % 10 + '0');
+	}
+	else if (tmp >= 1000 && tmp < 10000)
+	{
+		if (n < 0)
+			_putchar('-');
+		_putchar((tmp / 1000) + '0');
+		_putchar(((tmp / 100) % 10) + '0');
+		_putchar(((tmp / 10) % 10) + '0');
+		_putchar(tmp % 10 + '0');
+	}
+}
