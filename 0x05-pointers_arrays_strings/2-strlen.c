34c35935e853b70148381ad5e7bbe4e81cb9995b
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2022-12-19 15:21:44 -0800

    get the length of string without using strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ *
+ * @s: pointer to a char
+ * Description: Gets the length of the string passed in as s
+ *
+ * Return: Return the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while(*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
