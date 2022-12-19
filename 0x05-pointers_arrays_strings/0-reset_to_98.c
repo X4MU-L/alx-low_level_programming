5acdd89277e4e31d0be3e1ca7674796084b69d73
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2022-12-19 14:50:45 -0800

    reset pointer to an int to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * @n: Pointer to an int
+ * Description: Sets the pointer to int n to 98
+ *
+ * Return: Return (void)
+ */
+
+void reset_to_98(int *n)
+{
+	int m;
+
+	m = 98;
+	*n = m;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# Pointers and Strings
+
+This folder contains introductary projects to pointers and strings in C
