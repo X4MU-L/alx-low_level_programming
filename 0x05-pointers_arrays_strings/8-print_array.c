062eda2d382cb896c5912204ea904a65725468e7
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2022-12-19 18:45:02 -0800

    print n given element of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_array - Entry point
+ *
+ * @a: pointer to a char
+ * @n: int - number of array element to print
+ * Description: Prints the n element of array a
+ *
+ * Return: Return (void)
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i + 1 == n)
+		{
+			printf("%d\n", *(a + i));
+		}
+		else
+		{
+			printf("%d, ", *(a + i));
+		}
+	}
+}
