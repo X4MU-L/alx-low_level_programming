5fd230e67d81b56e85c92d44786f10e45f5f9ffe
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2022-12-28 03:12:58 -0800

    encode strings in rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,29 +11,32 @@
 char *rot13(char *str)
 {
 	int index = 0, index2;
-	char alphabet[26] = {'A', 'B', 'C', 'D', 'E', 'F',
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 			     'G', 'H', 'I', 'J', 'K', 'L',
 			     'M', 'N', 'O', 'P', 'Q', 'R',
 			     'S', 'T', 'U', 'V', 'W', 'X',
-			     'Y', 'Z'};
-	char rot13key[26] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 			     'T', 'U', 'V', 'W', 'X', 'Y',
 			     'Z', 'A', 'B', 'C', 'D', 'E',
 			     'F', 'G', 'H', 'I', 'J', 'K',
-			     'L', 'M'};
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
 
 	while (str[index])
 	{
-		for (index2 = 0; index2 < 26; index2++)
+		for (index2 = 0; index2 < 52; index2++)
 		{
 			if (str[index] == alphabet[index2])
 			{
 				str[index] = rot13key[index2];
-				break;
-			}
-			else if (str[index] == (alphabet[index2] + 32))
-			{
-				str[index] = (rot13key[index2] + 32);
 				break;
 			}
 		}
