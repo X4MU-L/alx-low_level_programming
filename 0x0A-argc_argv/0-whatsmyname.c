77af74f24b6ce061ad3bede51536993a32ca8f1b
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-06 15:16:13 -0800

    print number of argument to function call

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,9 +10,9 @@
  * Return: Returns success (0) always
  */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
