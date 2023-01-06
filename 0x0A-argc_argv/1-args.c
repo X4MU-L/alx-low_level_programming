43a8aeded76f44065e7fa43f41cecf2250928d5f
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-06 15:18:39 -0800

    print number of argument to function call

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: int - argument count supplied to the program
+ * @argv: char - pointer to an array of strings (arguments)
+ * Description: Prints the number of arguments supplied upon function call
+ *
+ * Return: Returns success (0) always
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
