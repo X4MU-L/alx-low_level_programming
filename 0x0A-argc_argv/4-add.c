dba48fe3b710dd58b6d4d1bae63008da05934bbe
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-06 16:15:48 -0800

    create a function to add all int arguments to a function call or return error on char

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,23 +14,27 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int num, digit, sum = 0;
 
 	if (argc < 2)
 	{
 		printf("%d\n", sum);
 		return (0);
 	}
-	for (i = 1; i < argc; i++)
-	{
-		if (!(atoi(argv[i])))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += atoi(argv[i]);
-	}
 
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[num]);
+	}
 	printf("%d\n", sum);
 	return (0);
 }
