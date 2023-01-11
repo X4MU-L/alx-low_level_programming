656e02e8077e48eb326764f33a23ee37a1aadf1f
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-10 16:12:28 -0800

    create function to dynamically allocate memory for 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,21 +2,23 @@
 #include <stddef.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory, which\
- * contains a copy of the string given as a parameter
+ * alloc_grid - returns a pointer to a newly allocated space in memory, for\
+ * 2D array and initailize to zero
  *
- * @str: pointer to a string of char
+ * @width: size of the array of int
+ * @height: size of the array of arrays
  *
- * Return: Returns a pointer to the copied string
+ * Return: Returns a pointer to the array or NULL on failure
  */
 
 int **alloc_grid(int width, int height)
 {
 	int **array, i, j;
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = malloc(sizeof(int) * height);
+	array = malloc(sizeof(int *) * height);
 
 	if (array == NULL)
 		return (NULL);
@@ -26,7 +28,12 @@ int **alloc_grid(int width, int height)
 		array[i] = malloc(sizeof(int) * width);
 
 		if (array[i] == NULL)
-			free(array[i]);
+		{
+			for (; i >= 0; i--)
+				free(array[i]);
+			free(array);
+			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
