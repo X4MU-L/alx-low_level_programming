58cbedabd1592308af19af8aa56a258fd419b32c
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-12 08:02:28 -0800

    create an array of range to max value

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates array an initize from min to max both included
+ *
+ * @min: minimum number in array
+ * @max: maximum number in array
+ *
+ * Return: Return pointer to array
+ */
+
+int *array_range(int min, int max)
+{
+	int size, i;
+	int *ptr_int;
+
+	if (min > max)
+		return (NULL);
+	size = (max - min) + 1;
+
+	ptr_int = malloc(sizeof *ptr_int * size);
+
+	if (ptr_int == NULL)
+		return (NULL);
+
+	for (i = 0; i <= max; i++)
+		ptr_int[i] = min++;
+
+	return (ptr_int);
+}
