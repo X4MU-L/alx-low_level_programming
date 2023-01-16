916834d0ff570853f457698bc0eefbb08cc75908
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-16 13:52:36 -0800

    define a typedef for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,12 @@ struct dog
 	float age;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
