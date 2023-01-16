ab2a40d53a54cc59579dd2935f42c6e088f88932
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-16 15:10:02 -0800

    free all struct dog memory allocation

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,10 +10,9 @@
 
 void free_dog(dog_t *d)
 {
-	if (d->name != NULL)
-		free(d->name);
-	if (d->owner != NULL)
-		free(d->owner);
-	if (d != NULL)
-		free(d);
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
