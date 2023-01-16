da7185f2b84e5d07c45e0bf258a64aa3cb2012b4
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-16 13:42:41 -0800

    print the values of struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,15 +13,15 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	if(d->name)
+	if (d->name != NULL)
 		printf("Name: %s\n", d->name);
 	else
 		printf("Name: (nil)\n");
-	if(d->age)
+	if (d->age < 0)
 		printf("Age: %f\n", d->age);
 	else
 		printf("Age: (nil)\n");
-	if(d->owner)
+	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
 	else
 		printf("Age: (nil)\n");
