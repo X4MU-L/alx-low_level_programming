6f008aed3c5efb388363eab8b9445d62ff00a6f6
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-18 05:31:50 -0800

    call a function on all numbers of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Takes in a function and iterate an array and call function\
+ * on each int
+ * @action: function pointer to a function that takes in an int and returns void
+ * @array: pointer to an array of ints
+ * Return: Returns void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
