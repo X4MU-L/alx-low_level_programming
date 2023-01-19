5d4d91aa5c85c6bcf3f287946e18b1db30877024
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-19 13:16:13 -0800

    print number of argument separated by a separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,15 +12,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-	if (separator == NULL || n == 0)
+	if (n == 0)
 		return;
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 			printf("%d%c ", va_arg(ap, int), *separator);
+		else if (i != (n - 1) && separator == NULL)
+			printf("%d ", va_arg(ap, int));
 		else
 			printf("%d\n", va_arg(ap, int));
 	}
