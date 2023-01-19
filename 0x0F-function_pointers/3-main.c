1c13000b84b1330aab8a552c35625a41f743680b
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-18 17:00:39 -0800

    create a function that returns a function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,20 +8,22 @@
  * Return: returns 0 on success otherwise exit with an exit code
  */
 
-int main(int __attribute__((__unused__)) argc, char **argv)
+int main(int argc, char **argv)
 {
 	int (*ptr_fnc)(int, int);
 	char *op;
 	int num1, num2;
 
-	op = argv[2];
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	op = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
