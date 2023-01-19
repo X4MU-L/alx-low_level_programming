db9e0ef1254669921dff2b83c21137e280d1f320
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-19 13:05:12 -0800

    print number of argument separated by a separator

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,8 +2,10 @@
 #define VARIADIC_H
 
 #include <stdarg.h>
+#include <stdio.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_H */
