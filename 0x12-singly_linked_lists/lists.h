c9d9f5c0f94ec5048f394d88160585f3395eafed
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-28 10:38:43 -0800

    (feat) print strings from a singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -13,9 +14,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
