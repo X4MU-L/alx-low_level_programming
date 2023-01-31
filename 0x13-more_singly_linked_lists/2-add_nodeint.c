3ce7225c22fc4d29ab0cd91af8cc2318edade548
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-30 18:27:05 -0800

    (fix: betty style) add function description

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * listint_len - gets the length of a linked list
- * @h: pointer to a struct listint
- * Return: returns the number of linked link
+ * add_nodeint - create a new node to a single linked list
+ * @head: pointer to a pointer to a struct listint
+ * @n: int
+ * Return: returns the address of the newly created node
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
