f5f7138e5a8cd8d6a3f9e21d3968853122459b41
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-31 09:40:50 -0800

    (feat) insert node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,40 +3,41 @@
 /**
  * insert_nodeint_at_index - sum all the data of a linked list
  * @head: pointer to a pointer to a struct listint
- * Return: returns the sum of all the data
+ * @idx: index at which to insert
+ * @n: int
+ * Return: returns the address of new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *new_node;
-	unsigned int len, i;
+	unsigned int i;
 
 	if (head == NULL)
 		return (NULL);
 
+	if (*head == NULL && idx != 0)
+		return (NULL);
 	temp = *head;
+	if (idx != 0)
+	{
+		for (i = 0; temp != NULL && i < (idx - 1); i++)
+			temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	if (temp == NULL && idx == 0)
+	if (idx == 0)
 	{
-		new_node->next = NULL;
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	if (temp != NULL && idx == 0)
-	{
-		new_node->next = temp;
-		*head = new_node;
-		return (new_node);
-	}
-	len = (unsigned int)listint_len(temp);
-	for (i = 0; temp->next != NULL && i < (idx - 1) && i < len; i++)
-	{
-		temp = temp->next;
-	}
+
 	new_node->next = temp->next;
 	temp->next = new_node;
 
