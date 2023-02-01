a1d3d6c2fd6350f5474097c57e5e8e0f15aa7091
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-01-31 23:48:42 -0800

    (feat) print linked list safely without chasing tails

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a linked list safely without chasing tails
+ * @head: pointer to the first node in the list
+ * Return: number of list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t len;
+	long int diff;
+
+	while (head)
+	{
+		diff = head - head->next;
+		len++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next,
+			       head->next->n);
+			break;
+		}
+	}
+
+	return (len);
+}
