9cb5c2748cc811d61993b7e8763e8a0630192437
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-02-01 00:05:50 -0800

    (feat) free a linked list safely

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a linked list safely without chasing tails
+ * @head: pointer to a pointer to the first node in the list
+ * Return: number of list
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	listint_t *temp;
+	size_t len;
+	long int diff;
+
+	while (*head)
+	{
+		diff = *head - (*head)->next;
+		temp = (*head)->next;
+		free(*head);
+		len++;
+
+		if (diff > 0)
+			*head = temp;
+		else
+			break;
+	}
+
+	*head = NULL;
+	return (len);
+}
