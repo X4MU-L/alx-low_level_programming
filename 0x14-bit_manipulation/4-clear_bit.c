262e7db706ae7d4c269c5e16e0fa1f9104aed5e9
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-02-03 12:12:14 -0800

    (feat) set the bit o a given index to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,9 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > (sizeof(unsigned long int) * 8 - 1))
+	if (index > sizeof(n) * 8)
 		return (-1);
-	*n = ((((*n >> index) & ~1) << index) & *n);
+	    *n &= ~(1 << index);
 
 	return (1);
 }
