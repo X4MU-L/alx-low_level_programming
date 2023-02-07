cfafdfc6764b43117d874d3e137f5111710f33cf
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-02-07 08:40:41 -0800

    (feat) create a new file or truncate an existing with permission rw- --- ---

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,9 @@ int create_file(const char *filename, char *text_content)
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	while (text_content[len])
-	       len++;
+	if (text_content != NULL)
+		while (text_content[len])
+			len++;
 
 	if (op != -1)
 		wr = write(op, text_content, len);
