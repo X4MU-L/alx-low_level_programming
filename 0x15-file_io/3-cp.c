905d72bc079adaf7abc35f3264f10c74a983d550
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-02-07 11:34:12 -0800

    (feat) copy a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -49,5 +49,11 @@ int main(int argc, char **argv)
 			close(op1);
 		}
 	}
+	if (n == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]);
+		exit(98);
+	}
 	return (1);
 }
