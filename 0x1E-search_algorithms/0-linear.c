99ae1b9867095c56b4e7a4bd22709af801e388eb
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-11 00:21:55 +0100

    (feat) -> implement a linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+
+/**
+ * print_search_info - logs the search info of a search algorithm
+ *
+ * @index - index of an array search
+ * @value - the value at @index
+ * @flag - a 1 or 0 flag
+ * @return - returns 1 or 0
+ */
+int print_search_info(size_t index, size_t value, int flag)
+{
+	if (flag)
+		return (1);
+	printf("Value checked array[%ld] = [%ld]\n", index, value);
+	return (0);
+}
+
+/**
+ * linear_search - searches for a value in an array using a linear
+ *                                                 search algorithm
+ * @array - the array to search linearly
+ * @value - the value to search for
+ * @size  - the size of the array
+ * @return - index of value if found else -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (print_search_info(i, array[i], (array[i] == value)))
+			return (i);
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,15 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+    AUTHOR: okoli chukwuebuka
+    email: okolisamuel21@gmail.com
+    github: x4mu-l
+*/
+
+int linear_search(int *array, size_t size, int value);
+
+#endif /*SEARCH_ALGOS*/
