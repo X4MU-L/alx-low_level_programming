dc058d77b50258f91d087231deae43ccb812e375
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-12 19:48:32 +0100

    (refactor) -> refator print function

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,22 +1,5 @@
 #include "search_algos.h"
 
-
-/**
- * print_search_info - logs the search info of a search algorithm
- *
- * @index: - index of an array search
- * @value: - the value at @index
- * @flag: - a 1 or 0 flag
- * Return: - returns 1 or 0
- */
-int print_search_info(size_t index, size_t value, int flag)
-{
-	if (flag)
-		return (1);
-	printf("Value checked array[%ld] = [%ld]\n", index, value);
-	return (0);
-}
-
 /**
  * linear_search - searches for a value in an array using a linear
  *                                                 search algorithm
@@ -32,7 +15,10 @@ int linear_search(int *array, size_t size, int value)
 	if (!array || (!*array && *array != 0))
 		return (-1);
 	for (i = 0; i < size; i++)
-		if (print_search_info(i, array[i], (array[i] == value)))
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
 			return (i);
+	}
 	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,8 +8,23 @@
  *   AUTHOR: okoli chukwuebuka
  *   email: okolisamuel21@gmail.com
  *   github: x4mu-l
-*/
+ */
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
