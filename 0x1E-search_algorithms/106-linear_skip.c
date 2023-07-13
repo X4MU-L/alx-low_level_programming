cefa231e0791549648eaedc68f07649bc530ee01
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-13 03:42:14 +0100

    (feat) -> skip list search algorithm

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - implement a linear skip search
+ *
+ * @list: - the linked list to search for @value
+ * @value: - the value to search for
+ * Return: - returnsthe node of value if found else NULL
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+
+	if (!list || value <= 0)
+		return (NULL);
+
+	return (list);
+}
